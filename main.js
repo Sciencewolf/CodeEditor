08c996cf66a31e291bc1120072da569484d39e58
Sciencewolf <aron17marton@gmail.com>
2022-12-10 08:58:50 +0100

    Initial commit

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 const appendLine = (event) => {
     const div_el = document.getElementById('inputs')
+    const span_el = document.getElementById('span-content')
     const textArea = document.getElementById('textarea')
-    const h4_counter = document.getElementById('counter')
+    const p_counter = document.getElementById('counter')
+
     let count = 0
-    let newh4 = ""
+    let newp = ""
 
     if(event.keyCode === 13) {
         textArea.rows++
+        textArea.cols = 150
         count++
         console.log("Count: ", count)
 
         //newh4 = document.createElement('h4')
-        h4_counter.innerHTML = count
+        p_counter.innerHTML = count
 
         //newh4.innerHTML = count
         //div_el.appendChild(newh4)
-        console.log("Log: ", h4_counter.innerHTML)
+        console.log("Log: ", p_counter.innerHTML)
     }
 
-    if(event.keyCode === 8 && textArea.value.length === 0) {
+    if(event.keyCode === 8 && textArea.value.length < 2) {
         textArea.rows--
         count--
         console.log("Count: another", count)
-        h4_counter.innerHTML = count
-        console.log("Log: another", h4_counter.innerHTML)
-        newh4.innerHTML = ""
+        p_counter.innerHTML = count
+        console.log("Log: another", p_counter.innerHTML)
+        newp.innerHTML = ""
 
     }
-
-
 }
 
 
