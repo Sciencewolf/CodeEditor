485f4a7b59f684b052d89edb4f6c2245a1996413
Sciencewolf <aron17marton@gmail.com>
2022-12-12 13:59:41 +0100

    Added tags on load, fixed counter

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
-let count = 0
+let count = 1
+let KEYWORDS = ["chislo", "holovna", "kity", "kity_net", "ajbo", "nekaj", "vhod"]
+
+const getTagsOnLoad = () => {
+    const span_el = document.getElementById('span-content')
+
+    let p_tag = document.createElement('p')
+    p_tag.id = "counter"
+    p_tag.innerHTML = count 
+
+    let input_tag = document.createElement('input')
+    input_tag.id = "input-field"
+    input_tag.type = "text"
+    input_tag.className = "input-field"
+    input_tag.placeholder = "To make a new line press [ENTER]"
+
+    span_el.appendChild(p_tag)
+    span_el.appendChild(input_tag)
+
+    // console.log("from getTagsOnLoad function")
+
+
+    const btns = document.getElementById('btns')
+    let button_save = document.createElement('button')
+    let button_copy = document.createElement('button')
+
+    button_save.id = "btn-save"
+    button_save.className = "btn-save"
+    button_save.innerHTML = "Save"
+    button_save.onclick = saveFile()
+
+    button_copy.id = "btn-copy"
+    button_copy.className = "btn-copy"
+    button_copy.innerHTML = "Copy"
+    button_copy.onclick = copyToClipboard()
+
+    btns.appendChild(button_copy)
+    btns.appendChild(button_save)
+
+}
 
 const appendOrDeleteLine = (event) => {
     const div_el = document.getElementById('inputs')
-    const p_counter = document.getElementById('counter')
 
     if(event.keyCode === 13) {
         count++
@@ -29,38 +67,33 @@ const appendOrDeleteLine = (event) => {
         new_input.focus()
         
     }
-
-    if(event.keyCode === 8) {
-        count--
+    else if(event.keyCode === 8 && count > 0) {
         console.log("Count: another", count)
+        if (count < 2) count = 1
+        else {
+            const get_elem_span = document.getElementById(`span-content${count}`)
 
-        const get_elem_div = document.getElementById('inputs')
-        const get_elem_span = document.getElementById(`span-content${count}`)
+            div_el.removeChild(get_elem_span)
+            count--
 
-        get_elem_div.removeChild(get_elem_span)
-
+            const last_input = document.querySelector(`.span-content${count} > input`)
+            last_input.focus()
+        }
     }
 }
 
 
 const changeColor = () => {
+    console.log('From changeColor function')
 
-    let KEYWORDS = ["chislo", "holovna", "kity", "kity_net", "ajbo", "nekaj", "vhod"]
-
-    const div_el = document.getElementById('inputs')
-    const input_field = document.getElementById('input-field')
-
-    if (input_field.value in KEYWORDS) {
-        input_field.style.color = "red"
-        
-        console.log(KEYWORDS)
-    }
-    else textArea.style.color = "black"
+    const input_field = document.getElementById(`input-field${count}`)
+    
 }
 
 
 // On button press copy all text
 const copyToClipboard = () => {
+    console.log("From copy func")
 
 }
 
