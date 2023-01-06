72a3cdc820115c5b1da7077db9ec20df0cde0684
Sciencewolf <aron17marton@gmail.com>
2023-01-06 23:17:34 +0100

    Initial commit

--- js/settings.js
+++ js/settings.js
@@ -1,2 +1,20 @@
 const log = console.log
 const error = console.error
+
+const themes = {
+    Black        : "#000" ,
+    Dark         : '#414A4C' ,
+    Melon        : "#EBB3A9" ,
+    Tea_Green     : "#D1F0B1" ,
+    Blue_Sapphire : "#086788" ,
+    Ming         : "#407076" ,
+    Jet          : "#39393A" ,
+    Floral_White  : "#FFFCF2"
+}
+
+const listDefaultFileExt = [
+    ".txt", ".html", ".css", ".js", ".ts",
+    ".cs", ".py", ".cpp", ".java", ".go",
+    ".pozpp"
+]
+
